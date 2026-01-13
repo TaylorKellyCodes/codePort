ee35a4f138bd61829c43339d36ee67e069a07eca
Taylor <Taylorkellycodes@gmail.com>
2026-01-13 09:00:55 -0500

    First draft

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -249,3 +249,12 @@
 	init.inertState();
 	init.eventListeners();
 })();
+
+const faqItems = Array.from(document.querySelectorAll('.cs-faq-item'));
+        for (const item of faqItems) {
+            const onClick = () => {
+            item.classList.toggle('active')
+        }
+        item.addEventListener('click', onClick)
+        }
+                                
